94cfca883b7b295bc0d4175764ba7f1d268a4ba5
ali-osaid01 <aliusaid55@gmail.com>
2024-12-17 18:57:53 +0500

    feat:setting update

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,9 +2,9 @@ import { Response } from "express";
 
 export const setAccessTokenCookie = (res: Response, accessToken: string) => {
     res.cookie('accessToken', accessToken, {
-        httpOnly: false, 
-        secure: false, 
-        sameSite: 'lax',
+        httpOnly: true, 
+        secure: true, 
+        sameSite: 'none',
         path: '/', 
     });
 };
